3726e575e9fe42af36c4a1f85eb802c0167f72cb
Pedro Cantarelli Costa <pedro.costa@atlastechnol.com>
2021-04-21 15:27:37 -0300

    :construction:  Change repositories path file

--- src/repositories/pokemonRepository.js
+++ src/repositories/pokemonRepository.js
@@ -1,11 +1,14 @@
 import { Repository } from './repository';
 
-const MAX_POKEMONS = 797;
+const MAX_POKEMONS = 150;
 const resource = (path = '') => `/pokemon${path}`;
 
 export default {
   async list() {
     return Repository.get(resource(`?limit=${MAX_POKEMONS}`));
+  },
+  async getByName(name) {
+    return Repository.get(resource(`/${name}`));
   },
   async getById(id) {
     return Repository.get(resource(`/${id}`));
